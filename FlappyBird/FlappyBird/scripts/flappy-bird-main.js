792866810b9aa821b57fe732c56940e75f2248a9
bugsancho <bugsancho@gmail.com>
2014-06-07 17:58:08 +0300

    added ninja placeholder

--- FlappyBird/FlappyBird/scripts/flappy-bird-main.js
+++ FlappyBird/FlappyBird/scripts/flappy-bird-main.js
@@ -1,33 +1,81 @@
 ﻿/// <reference path="kinetic-v5.1.0.min.js" />
 window.onload = function () {
-    var stage, 
+    var stage,
     gameSpeed,
     currentState,
-    currentPlayer, //ninja 
+    ninja, //ninja 
     obstacles = [], //pipes
-    background,
+    backgroundLayer,
+    ninjaLayer,
+    obstaclesLayer,
     currentScore,
     highScore,
     gravity,
     playerJumpAcceleration,
-    gameStates = {  
-         InGame: 1,
-         HighScores: 2,
-         Pause: 3,
-         GameOver: 4,
+    gameStates = {
+        InGame: 1,
+        HighScores: 2,
+        Pause: 3,
+        GameOver: 4,
     };
 
-    Initalize(400, 500);    
+    initalize(400, 500);
 
-    function Initalize(width, height) {
+    function Ninja(x, y) {
+        this.x = x,
+        this.y = y,
+        this.jumpLength = 60,
+        this.img = new Kinetic.Rect({
+            x: this.x,
+            y: this.y,
+            width: 50,
+            height: 50,
+            fill: 'green',
+            stroke: 'black',
+            strokeWidth: 4
+        })
+        this.jump = function () {
+            this.y -= this.jumpLength;
+            if (this.y  < 0) {
+                this.y = 0;
+            }
 
-        stage = new Kinetic.Stage({ 
-            container: 'container',
-            width: width,
-            height: height
-        });
-        var ninjaLayer = new Kinetic.Layer();
-        stage.add(ninjaLayer);        
+        }
+        this.update = function () {
+
+            if (this.y + this.img.height() < stage.height()) {
+                this.y += 3;
+            }
+
+        }
     }
 
+    var ninjaAnimation = new Kinetic.Animation(update, ninjaLayer);
+    ninjaAnimation.start();
+    document.addEventListener('click', function () {
+        ninja.jump();
+    });
+
+    function initalize(width, height) {
+
+        stage = new Kinetic.Stage({
+            container: 'container',
+            width: width,
+            height: height,
+        });
+
+        ninjaLayer = new Kinetic.Layer();
+        backgroundLayer = new Kinetic.Layer();
+
+        ninja = new Ninja(75, 150);
+        ninjaLayer.add(ninja.img);
+        stage.add(ninjaLayer);
+    }
+
+    function update() {
+        //obstacle update
+        ninja.update();
+        ninja.img.setY(ninja.y);
+
+    }
 }
