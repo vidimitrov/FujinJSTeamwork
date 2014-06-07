44f0ee47b6322d7b632d527a8b311b8b88bbd801
bugsancho <bugsancho@gmail.com>
2014-06-07 15:34:24 +0300

    added inital states

--- FlappyBird/FlappyBird/scripts/flappy-bird-main.js
+++ FlappyBird/FlappyBird/scripts/flappy-bird-main.js
@@ -1,5 +1,33 @@
 ﻿/// <reference path="kinetic-v5.1.0.min.js" />
-/// <reference path="raphael-min.js" />
 window.onload = function () {
+    var stage, 
+    gameSpeed,
+    currentState,
+    currentPlayer, //ninja 
+    obstacles = [], //pipes
+    background,
+    currentScore,
+    highScore,
+    gravity,
+    playerJumpAcceleration,
+    gameStates = {  
+         InGame: 1,
+         HighScores: 2,
+         Pause: 3,
+         GameOver: 4,
+    };
+
+    Initalize(400, 500);    
+
+    function Initalize(width, height) {
+
+        stage = new Kinetic.Stage({ 
+            container: 'container',
+            width: width,
+            height: height
+        });
+        var ninjaLayer = new Kinetic.Layer();
+        stage.add(ninjaLayer);        
+    }
 
 }
